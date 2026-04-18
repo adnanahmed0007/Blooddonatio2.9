8355b477d8d471001a1dea8a31e68c93d701ef4b
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-04-18 15:54:47 +0530

    aded

--- Server/routes/Service/DonationRoutes.js
+++ Server/routes/Service/DonationRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 import Donation from "../../controllers/Donation.js";
 import Search from "../../controllers/Search.js";
 import verifyJwt from "../../middleware/VerifyJwt.js";
-
+import FindAllBlood from "../../controllers/FIndAllblood.js";
 const routerDonation = express.Router();
 routerDonation.post("/donation/api/register", verifyJwt, Donation);
 routerDonation.post("/donation/api/get", verifyJwt, Search);
+routerDonation.get("/donation/api/getall", FindAllBlood)
 
 export default routerDonation;
