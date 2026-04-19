627afc201e21dc1860bb62b114a562d510a40c40
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-04-19 16:54:33 +0530

    added

--- Server/routes/Service/DonationRoutes.js
+++ Server/routes/Service/DonationRoutes.js
@@ -6,6 +6,6 @@ import FindAllBlood from "../../controllers/FIndAllblood.js";
 const routerDonation = express.Router();
 routerDonation.post("/donation/api/register", verifyJwt, Donation);
 routerDonation.post("/donation/api/get", verifyJwt, Search);
-routerDonation.get("/donation/api/getall", FindAllBlood)
+routerDonation.get("/donation/api/getall", verifyJwt, FindAllBlood)
 
 export default routerDonation;
