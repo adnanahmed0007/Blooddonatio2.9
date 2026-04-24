ff63f94e02039b1e152bd50bbaf5e5405cd2460f
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-04-24 15:28:28 +0530

    added

--- Server/routes/Authentication/Authe.js
+++ Server/routes/Authentication/Authe.js
@@ -3,10 +3,12 @@ import Signupuser from "../../controllers/Signup.js";
 import Login from "../../controllers/Login.js";
 import Logout from "../../controllers/Logout.js";
 import verifyJwt from "../../middleware/VerifyJwt.js";
+
 const router = express.Router();
 router.post("/signup", Signupuser);
 
 router.post("/login", Login)
 router.get("/logout", verifyJwt, Logout)
+
 
 export default router;
