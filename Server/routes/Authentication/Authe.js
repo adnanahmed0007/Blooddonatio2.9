3c09e41c45595ede791c10521ae047ffa584799a
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-04-29 16:37:27 +0530

    Add check route to Authentication

--- Server/routes/Authentication/Authe.js
+++ Server/routes/Authentication/Authe.js
@@ -4,8 +4,10 @@ import Login from "../../controllers/Login.js";
 import Logout from "../../controllers/Logout.js";
 import verifyJwt from "../../middleware/VerifyJwt.js";
 import Profile from "../../controllers/Profile.js";
+ import check from "../../controllers/Check.js";
 const router = express.Router();
 router.post("/signup", Signupuser);
+router.get("/me", verifyJwt, check)
 
 router.post("/login", Login)
 router.get("/logout", verifyJwt, Logout)
